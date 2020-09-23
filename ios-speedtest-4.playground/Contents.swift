561f63b97b727398860e92c1664fb672a1aba7f3
bodour Alrashidi <bodouralrashidi@gmail.com>
2020-09-23 06:24:15 +0300

    replace repo

---
+++ ios-speedtest-4.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+
+
+//: # 1️⃣ for-in loop
+
+//MARK: for- in loop خمس مرات باستخدام ال "iOS" قم بطباعه
+
+
+
+
+//: # 2️⃣ function
+
+// MARK: هذي الداله تقوم بضرب رقمين و ترجع الناتج
+
+func multiplication(  number1 : Int ,  number2 : Int)
+{
+    
+    return number1 * number2
+}
+// MARK:   بما يناسب صيغه هذا الاستدعاء multiplication قم بتعديل  الداله
+
+multiplication(5,7)
+
+
+
+
+//: # 3️⃣ struct
+// struct...
+
+
+
+// MARK:  الكائن  object مبنية علي هذي struct  قم بكتابة كود ال
+var student = Student(name: "Omar", age: 25 , hobbies: ["Coding", "drawing"])
