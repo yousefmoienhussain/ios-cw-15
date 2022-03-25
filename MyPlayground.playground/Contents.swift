2acca0c708e10a6a028a0b70ce3755d0f4603893
yousef moien hussain <zeroyousef13@gmail.com>
2022-03-25 20:45:28 +0300

    cw15

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct student {
+    var name: String
+    var age: Int
+    var grade: Double
+    var atendence: Bool
+}
+
+var yousef = student(name: "yousef", age: 18, grade: 70, atendence: true)
